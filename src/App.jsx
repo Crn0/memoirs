9d704826127b9f8b718859d6c6da8affe5b073c3
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-21 19:32:48 +0800

    feat: usecontext and outlet on app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
+import ThemeContext from './context/themeContext';
+import UserContextContext from './context/userContext';
+import Header from './components/ui/header';
+
+function App() {
+    const data = useLoaderData();
+    const [user, setUser] = useState(data);
+    const [theme, setTheme] = useState('dark');
+
+    return (
+        <>
+            <ThemeContext.Provider value={{ theme, setTheme }}>
+                <UserContextContext.Provider value={{ user, setUser }}>
+                    <Header />
+
+                    <main className={`${theme}`}>
+                        <Outlet />
+                    </main>
+                    
+                </UserContextContext.Provider>
+            </ThemeContext.Provider>
+        </>
+    );
+}
+
+export default App;
