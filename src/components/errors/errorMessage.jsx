65469f6e3e8883cdac965077efd7563710ec0a18
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-21 19:31:17 +0800

    feat: main error component

---
+++ src/components/errors/errorMessage.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export default function ErrorMessage({ message }) {
+    return <p className='error__message'>{ message }</p>
+}
+
+ErrorMessage.propTypes = {
+    message: PropTypes.string.isRequired,
+};
