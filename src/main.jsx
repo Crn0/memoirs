50401e3541984cf4c3cdfa0b6b337f91ffaedcf6
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-21 19:36:14 +0800

    refactor: format code

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
-import './global-styles/index.css'
-import './global-styles/reset.css'
-import './global-styles/theme.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './routes/router';
+import './global-styles/index.css';
+import './global-styles/reset.css';
+import './global-styles/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Router />
+    </React.StrictMode>
+);
