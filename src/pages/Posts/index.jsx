699e87e626e4aadadf7644b5004d156d080f0b8b
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-21 19:45:55 +0800

    feat: create Posts components

---
+++ src/pages/Posts/index.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import ThemeContext from "../../context/themeContext";
+import Card from "../../components/ui/card/card";
+
+export default function Posts() {
+    const { posts } = useLoaderData()
+    const { theme } = useContext(ThemeContext);
+
+    return (
+        <div className={`${ theme } posts__container`}>
+            {
+                (() => {
+                    if(posts?.length !== 0) {
+                        return (
+                            <>
+                                {
+                                    posts.map((post) => (<Card post={ post } key={ post._id }/>))
+                                }
+                            </>
+                        )
+                    }
+
+                    return <p>There are no posts.</p>
+                })()
+            }
+        </div>
+    );
+}
