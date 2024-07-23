6423313c7e5d6ec1a687a3c07491c85cb872908b
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-23 16:25:25 +0800

    fix: token request url

--- src/loaders/rootLoader.js
+++ src/loaders/rootLoader.js
@@ -8,7 +8,7 @@ export default async function loader() {
         if (authStorage.has('auth') === false) {
             if (localStorage.has('token')) {
                 const bearerToken = `Bearer ${localStorage.get('token')}`;
-                const res = await fetch(`${URL}/users/token`, {
+                const res = await fetch(`${URL}/users/token/me`, {
                     headers: { Authorization: `${bearerToken}` },
                 });
                 const data = await res.json();
