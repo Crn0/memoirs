044a6a6c22d2c947b1129bd75affc3bd4475264f
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-23 18:06:35 +0800

    feat: login route
    refactor form error message
    set user and token after a successful login
    user cannot access login route if they are
        authenticated

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import { useActionData, useNavigate } from 'react-router-dom';
+import { useActionData, useNavigate, useLocation } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
 import ThemeContext from '../../context/themeContext';
 import UserContext from '../../context/userContext';
@@ -8,104 +8,90 @@ import Label from '..//../components/ui/form/Label';
 import Input from '../../components/ui/form/Input';
 import Button from '../../components/ui/button/Button';
 import ErrorMessage from '../../components/errors/errorMessage';
+import fieldNameIncludes from '../../helpers/form/fieldnameIncludes';
+import fieldError from '../../helpers/form/fieldMessage';
+import FieldErrorMessage from '../../components/ui/form/FieldErrorMessage';
+import localStorage from '../../helpers/storage/localStorage';
 
 export default function Login() {
-    const loginData = useActionData();
     const { theme } = useContext(ThemeContext);
     const { setUser } = useContext(UserContext);
+    const loginData = useActionData();
+    const location = useLocation();
     const navigate = useNavigate();
+    
+    const error = loginData?.error;
+    const user = loginData?.user;
+    const token = loginData?.token;
+
 
     useEffect(() => {
-        if (!loginData?.error && loginData?.user) {
-            // setUser(loginData.user);
-            // navigate('/');
-            console.log('foobar', loginData instanceof Error);
-        }
-    }, [loginData, setUser, navigate]);
-    
-    const fieldError = (name) => {
-        const error = loginData?.error?.messages?.filter?.(error => error.field === name)
-        return error[0].message
-    }
-    const includes = (fieldName, array) => {
-        if(!Array.isArray(array)) return false;
 
-        const queue = [...array];
-        while(queue.length) {
-            const data = queue[0]
+        if(localStorage.has('token')) {
+            const { from } = location.state || { from: { pathname: "/" } };
 
-            if(data.field === fieldName) return true;
-
-            queue.shift();
+            return navigate(from, { replace: true })
         }
 
-        return false;
-    }
+        if (!error && user && token) {
+
+            localStorage.add('token', token);
+            setUser(user);
+
+            return navigate('/', { replace: true });
+        }
+
+    }, [user, token, setUser, navigate, error, location]);
 
     return (
-        <div className="page__container">
+        <div className="form__container">
                 {
-                    (() => {
-                        
-                        if((!includes('email', loginData?.error?.messages) && !includes('password', loginData?.error?.messages)) && loginData?.error) {
-                            const { messages } = loginData.error
-
+                    error && (() => {
+                        const { messages } = error;
+                        const noFieldErrors = (!fieldNameIncludes('email', messages) && !fieldNameIncludes('password', messages)) && error;
+                        if(noFieldErrors) {
                             return (
                                 <div className='error__container'>
                                     <ErrorMessage message={ messages }/>
                                 </div>
                             )
+                        } else {
+                            return ['email', 'password'].map((fName) => {
+                                return <FieldErrorMessage key={fName} fieldName={fName} error={error}/>
+                            })
                         }
                     })()
                 }
            
             <Form action="/login" method="POST">
-                <Fieldset>
+                <Fieldset fieldName='email'>
                     <Label theme={theme} name="Email">
-        
                         <Input
                             theme={theme}
                             type={'email'}
                             name={'email'}
                             uncontrolled={true}
                         />
-
-                        {
-                            (() => {
-                                if(includes('email', loginData?.error?.messages)) {
-                                    return <ErrorMessage message={  fieldError('email') } />
-                                }
-
-                            })() 
-                        }
-
                     </Label>
+                </Fieldset>
 
+                <Fieldset fieldName='password__field'>
                     <Label theme={theme} name="Password">
-
                         <Input
                             theme={theme}
                             type={'password'}
                             name={'password'}
                             uncontrolled={true}
                         />
-
-                        {
-                            (() => {
-                                if(includes('password', loginData?.error?.messages)) {
-                                    return <ErrorMessage message={  fieldError('password')  }/>;
-                                }
-                            })()
-                        }
-
                     </Label>
                 </Fieldset>
 
-                <Fieldset>
+                <Fieldset fieldName='button__field'>
                     <Button type={'submit'} size={'medium'} uncontrolled={true}>
                         Login
                     </Button>
                 </Fieldset>
+                
             </Form>
         </div>
     );
