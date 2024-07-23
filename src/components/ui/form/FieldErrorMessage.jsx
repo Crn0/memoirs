3a9041ea6947c88f9cd1c602486de9c0b9876acc
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-23 17:45:27 +0800

    feat(field): Error component
    Create Field error component
    Create field message getter
    Create function to check if field name is included
     in the error

---
+++ src/components/ui/form/FieldErrorMessage.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import ErrorMessage from "../../errors/errorMessage";
+import fieldMessage from "../../../helpers/form/fieldMessage";
+import fieldNameIncludes from "../../../helpers/form/fieldnameIncludes";
+
+export default function FieldErrorMessage({fieldName, error}) {
+    const messages = error?.messages
+    const clone = structuredClone(messages);
+
+    if(fieldNameIncludes(fieldName, clone)) return <ErrorMessage message={  fieldMessage(fieldName, clone) } />
+}
+
+FieldErrorMessage.propTypes = {
+    fieldName: PropTypes.string.isRequired,
+    error: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.object)).isRequired,
+};
