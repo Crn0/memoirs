e5b9dff2f55a8bda3685d7331702566263b90caf
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-24 15:38:35 +0800

    feat: custom hook to handle auth and navigation

---
+++ src/hooks/useAuthData.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import localStorage from "../helpers/storage/localStorage";
+
+export default function useAuthData(error, user, token, setUser) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(localStorage.has('token')) {
+            const { from } = location.state || { from: { pathname: "/" } };
+
+            return navigate(from, { replace: true })
+        }
+        
+        if(!error && user && token) {
+            console.log(!error,user,token)
+            localStorage.add('token', token);
+            setUser(user);
+
+            return navigate('/', { replace: true });
+        }
+    }, [error, user, token, setUser, navigate]);
+}
