9fb93cf39785038d73725c6ebd99840c32429893
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-28 14:27:01 +0800

    chore: add todo for feature

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -7,10 +7,15 @@ export default function Posts() {
     const { posts } = useLoaderData();
     const { theme } = useContext(ThemeContext);
 
+    /**
+     * TODO: add bookmark feature
+     */
+
+
     return (
         <div className={`${theme} posts__container`}>
             {(() => {
-                if (posts?.length !== 0) {
+                if (!posts?.length <= 0) {
                     return (
                         <>
                             {posts.map((post) => (
