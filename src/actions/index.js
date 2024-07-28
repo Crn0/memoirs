cbe98e01e0fa2ba35b92568719d068976c98c103
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-28 14:24:15 +0800

    feat(action/comment): add and delete comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import loginAction from './loginAction';
 import signUpAction from './signupAction';
+import commentAction from './commentAction';
 
 export default {
     loginAction,
     signUpAction,
+    commentAction,
 };
