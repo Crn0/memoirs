26d15dea54e7e80bb472370a514fb5a8e61e4a9c
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-29 14:24:33 +0800

    feat: show user name

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,3 +1,87 @@
+/* eslint-disable no-unused-vars */
+import { useContext, useReducer } from "react"
+import { Navigate, useParams } from "react-router-dom"
+import { DateTime } from "luxon";
+import UserContext from "../../context/userContext"
+import Form from "../../components/ui/form/Form";
+import FieldSet from "../../components/ui/form/Fieldset";
+import Label from "../../components/ui/form/Label";
+import Input from "../../components/ui/form/Input";
+import Button from "../../components/ui/button/Button";
+import reducer from "./reducer";
+
+
 export default function Profile() {
-    return <p>Profile page not implemented</p>
+    const { user } = useContext(UserContext);
+    const { userId } = useParams();
+    const [state, dispatch] = useReducer(reducer, {
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        email: user?.email,
+        username: user?.username,
+    });
+
+    const fullName = `${state?.firstName} ${state?.lastName}`;
+    const userName = `${state?.username}`;
+    const email = `${state?.email}`;
+    const date = DateTime.fromISO(user?.createdAt).toFormat('LLL dd yyyy');
+
+    return (
+        <>
+            {
+                (() => {
+                    if(user?._id === userId) {
+                        return (
+                            <>
+                                <section>
+                                    <div className="note">
+                                        <p>
+                                            note: some features are not yet implemented
+                                        </p>
+                                    </div>
+
+                                    <div className="profile">
+                                        <div className="profile__actions">
+                                            <Button
+                                                type="button"
+                                                size="medium"
+                                                disabled={ false }
+                                            >
+                                                Edit profile
+                                            </Button>
+                                        </div>
+
+                                        <div className="profile__details">
+                                            <div className="profile__top">
+                                                <h1>
+                                                    { `${fullName} (${userName})`}
+                                                </h1>
+                                            </div>
+
+                                            <div className="profile__middle">
+                                                <p> { email } </p>
+                                            </div>
+
+                                            <div className="profile__bottom">
+                                                <p> {`Joined on ${date}`}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+
+                                <section>
+                                    <h2>Bookmarks</h2>
+                                    <p>
+                                        You do not have bookmarked posts
+                                    </p>
+                                </section>
+                            </>
+                        );
+                    }
+
+                    return <Navigate to="/" replace={true} />
+                })()
+            }
+        </>
+    );
 }
---
+++ src/pages/Profile/reducer.js
@@ -0,0 +1,33 @@
+import formConstants from '../../constants/form';
+
+const reducer = (state, action) => {
+    const { type, value } = action;
+
+    switch(type) {
+        case formConstants['FIRST_NAME']: 
+            return {
+                ...state,
+                [type]: value,
+            };
+
+        case formConstants['LAST_NAME']:
+            return {
+                ...state,
+                [type]: value,
+            };
+
+        case formConstants['EMAIL']: 
+            return {
+                ...state,
+                [type]: value,
+            };
+
+        case formConstants['USERNAME']:
+            return {
+                ...state,
+                [type]: value,
+            };
+    }
+};
+
+export default reducer;
