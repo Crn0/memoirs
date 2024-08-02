cdbaed85b8b92038e9e653731f0569a638009984
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-02 17:59:23 +0800

    feat(spinner): add testid

--- src/components/ui/spinner/index.jsx
+++ src/components/ui/spinner/index.jsx
@@ -4,6 +4,7 @@ export default function Spinner({ message }) {
     return (
         <div className="spinner">
             <svg
+                data-testid="spinner"
                 xmlns="http://www.w3.org/2000/svg"
                 width="1em"
                 height="1em"
