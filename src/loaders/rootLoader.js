cb035820e9a523a7d0b841bf57a82e17021fc891
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-05 16:07:25 +0800

    refactor: remove auth storage and console log

--- src/loaders/rootLoader.js
+++ src/loaders/rootLoader.js
@@ -1,36 +1,28 @@
 import { redirect } from 'react-router-dom';
 import { URL } from '../constants/env';
 import localStorage from '../helpers/storage/localStorage';
-import authStorage from '../helpers/storage/authStorage';
 
 export default async function loader() {
     try {
-        if (authStorage.has('auth') === false) {
-            if (localStorage.has('token')) {
-                const bearerToken = `Bearer ${localStorage.get('token')}`;
-                const res = await fetch(`${URL}/users/token/me`, {
+        if (localStorage.has('token')) {
+            const bearerToken = `Bearer ${localStorage.get('token')}`;
+            const res = await fetch(`${URL}/users/token/me`, {
                     headers: { Authorization: `${bearerToken}` },
-                });
-                const data = await res.json();
+            });
+            const data = await res.json();
 
-                if (res.status >= 400) {
-                    throw new Error(data.message, { cause: data.errors });
-                }
-
-                return {
-                    user: data.user,
-                };
+            if (res.status >= 400) {
+                throw new Error(data.message, { cause: data.errors });
             }
 
-            return null;
+            return {
+                user: data.user,
+            };
         }
 
-        return authStorage.get('auth');
+        return null;
     } catch (error) {
-        console.log(error);
-
         localStorage.remove('token');
-
         return redirect('/login');
     }
 }
