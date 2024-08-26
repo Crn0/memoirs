0cf5c282affe1fad4231ff80884576aff4511e73
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-26 15:24:36 +0800

    feat: rickroll users if path is invalid

--- src/components/errors/main.jsx
+++ src/components/errors/main.jsx
@@ -2,20 +2,18 @@ import { useContext } from 'react';
 import { useRouteError } from 'react-router-dom';
 import Link from '../ui/link/Link';
 import ThemeContext from '../../context/themeContext';
+import rickRoll from '../../assets/rick-roll.gif';
+import style from './css/mainError.module.css';
 
 export default function MainError() {
     const error = useRouteError();
     const { theme } = useContext(ThemeContext);
 
     return (
-        <div className={`${theme}`}>
+        <div className={`${style.error}`} style={{backgroundImage: `url(${rickRoll})`}}>
             <h2>Oops</h2>
-            <p>Sorry, an unexpected error has occurred</p>
+            <p>Sorry, you just got rick rolled :P</p>
             <Link url="/">Home</Link>
-            <p>
-                <i>{error.status || error.code}</i>
-                <i>{error.statusText || error.message}</i>
-            </p>
         </div>
     );
 }
