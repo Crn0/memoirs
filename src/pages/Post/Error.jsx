6aeff7c55b287056572dd92db334e50726235d19
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-26 15:31:27 +0800

    style: center error message

--- src/pages/Post/Error.jsx
+++ src/pages/Post/Error.jsx
@@ -1,10 +1,11 @@
 import { useAsyncError } from 'react-router-dom';
+import style from './css/error.module.css';
 
 export default function PostError() {
     const error = useAsyncError();
-
+    
     return (
-        <div className="error">
+        <div className={`${style.error}`}>
             <h1 className="error__code">{error?.httpCode || error?.code}</h1>
             <p className="error__message">{error?.message}</p>
         </div>
